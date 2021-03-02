74ade1284ee64584d9d9eed84a1372a06fe100c1
Yordan Iliev <jordan.iliev501@gmail.com>
2021-03-02 17:13:24 +0200

    Make camera follow player smoothly

---
+++ Homework-1/Assets/Scripts/Follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToFollow;
+    [SerializeField] private float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(objectToFollow != null)
+        {
+            Vector2 dir = objectToFollow.transform.position - transform.position;
+            Vector2 dirScaled = dir * Time.deltaTime * speed;
+            transform.Translate(dirScaled);
+        }
+    }
+}
