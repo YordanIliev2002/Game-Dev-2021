4a5c8243d50206e4c779d7d67c18915e557c64f3
Yordan Iliev <jordan.iliev501@gmail.com>
2021-03-02 17:52:09 +0200

    Add horizontal movement for the player

---
+++ Homework-1/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float walkStrength = 3;
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float velocityX = Input.GetAxis("Horizontal") * walkStrength;
+        ApplyHorizontalMovement(velocityX);
+        UpdateFacingDirection(velocityX);
+    }
+
+    void ApplyHorizontalMovement(float velocityX)
+    {
+        body.velocity = new Vector2(velocityX, body.velocity.y);
+    }
+
+    void UpdateFacingDirection(float velocityX) 
+    {
+        if (velocityX > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(velocityX < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        // If we are not moving, we should not flip.
+    }
+}
