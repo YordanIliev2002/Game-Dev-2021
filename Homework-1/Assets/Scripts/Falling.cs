0aae852865d81e42b726b3c460706f141c11aa2c
Yordan Iliev <jordan.iliev501@gmail.com>
2021-03-05 23:24:22 +0200

    Add falling platforms

---
+++ Homework-1/Assets/Scripts/Falling.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling : MonoBehaviour
+{
+    public Rigidbody2D body;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            body.bodyType = RigidbodyType2D.Dynamic;
+            Destroy(gameObject, 1f);
+        }
+    }
+
+}
