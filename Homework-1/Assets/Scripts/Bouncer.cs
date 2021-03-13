9f81e6bcc93d12e793d67a59b4ace9d8dacd5f2e
Yordan Iliev <jordan.iliev501@gmail.com>
2021-03-13 19:48:16 +0200

    Add spring

---
+++ Homework-1/Assets/Scripts/Bouncer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+    public float bounciness = 1.5f;
+    public float velocityLimit = 11f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject other = collision.gameObject;
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody2D body = other.GetComponent<Rigidbody2D>();
+            body.velocity = new Vector2(body.velocity.x, Mathf.Min(velocityLimit, Mathf.Abs(bounciness * body.velocity.y)));
+        }
+    }
+}
