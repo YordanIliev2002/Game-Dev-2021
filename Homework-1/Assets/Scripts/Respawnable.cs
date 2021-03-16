2f33f0c25062d0ba51a3b91fe4a048e238289b4f
Yordan Iliev <jordan.iliev501@gmail.com>
2021-03-16 16:55:41 +0200

    Make player respawn after falling out of map

---
+++ Homework-1/Assets/Scripts/Respawnable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawnable : MonoBehaviour
+{
+    private Vector2 spawnPoint;
+    public int yThreshhold = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.y < yThreshhold)
+        {
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        gameObject.transform.position = spawnPoint;
+        Rigidbody2D body = gameObject.GetComponent<Rigidbody2D>();
+        if(body)
+        {
+            body.velocity = Vector2.zero;
+        }
+    }
+}
