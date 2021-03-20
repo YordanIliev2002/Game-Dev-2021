90229ee6ae6c3126575e05750ac119f39d724ce5
Yordan Iliev <jordan.iliev501@gmail.com>
2021-03-20 12:42:47 +0200

    Add gravity pill

---
+++ Homework-1/Assets/Scripts/GravityPill.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPill : MonoBehaviour
+{
+    private GameObject affectedPlayer;
+    [SerializeField]
+    private float duration = 1f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            affectedPlayer = collision.gameObject;
+            Take();
+            gameObject.SetActive(false);
+            Invoke("Take", duration);
+            Destroy(gameObject, duration + 1);
+        }
+    }
+
+    void Take()
+    {
+        Rigidbody2D body = affectedPlayer.GetComponent<Rigidbody2D>();
+        body.gravityScale = -body.gravityScale;
+    }
+
+}
