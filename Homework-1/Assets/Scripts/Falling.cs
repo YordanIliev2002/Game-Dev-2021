6ec3aef02cdf3c93bf0c2fca14cf34759687f5b1
Yordan Iliev <jordan.iliev501@gmail.com>
2021-03-20 12:11:45 +0200

    Make falling platforms respawn

--- Homework-1/Assets/Scripts/Falling.cs
+++ Homework-1/Assets/Scripts/Falling.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class Falling : MonoBehaviour
 {
+    public Vector2 startPosition;
     public Rigidbody2D body;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             body.bodyType = RigidbodyType2D.Dynamic;
-            Destroy(gameObject, 1f);
+            Invoke("Kill", 1);
+            Invoke("Respawn", 3);
         }
+    }
+
+    void Kill()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void Respawn()
+    {
+        gameObject.SetActive(true);
+        gameObject.transform.position = startPosition;
+        body.bodyType = RigidbodyType2D.Kinematic;
     }
 
 }
