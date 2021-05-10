c12502805970bd8317d4f3163078ec22ce8a22df
Yordan Iliev <jordan.iliev501@gmail.com>
2021-05-10 23:03:13 +0300

    Apply the new input system and add controller support.

--- Homework-1/Assets/Scripts/Player/CharacterMovement.cs
+++ Homework-1/Assets/Scripts/Player/CharacterMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
@@ -8,17 +9,59 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private float walkStrength = 3;
     [SerializeField] private Animator animator;
     [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private PlayerControls input;
+    private Vector2 inputVector = Vector2.zero;
+
     private Rigidbody2D body;
     private GroundChecker groundChecker;
     private Respawnable respawnable;
     private int countOfJumps = 0;
     private float lastJumpButtonState = 0;
+    private bool isMoveInvoked = false;
+
+    private void OnEnable()
+    {
+        input = new PlayerControls();
+        input.Enable();
+        input.Player.Movement.started += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        input.Player.Movement.started -= OnMove;
+        input.Disable();
+    }
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         groundChecker = GetComponentInChildren<GroundChecker>();
         respawnable = GetComponent<Respawnable>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (!isMoveInvoked)
+        {
+            StartCoroutine(Move());
+        }
+    }
+
+    public IEnumerator Move()
+    {
+        isMoveInvoked = true;
+        do
+        {
+            inputVector = input.Player.Movement.ReadValue<Vector2>();
+            if(respawnable.IsAlive()) // To prevent flipping when the player is dead.
+            {
+                UpdateFacingDirection(inputVector.x);
+            }
+            yield return null;
+        } while (inputVector.sqrMagnitude > 0);
+        isMoveInvoked = false;
+        inputVector = Vector2.zero;
     }
 
     void Update()
@@ -28,11 +71,12 @@ public class CharacterMovement : MonoBehaviour
             animator?.SetBool("isDead", true);
             return;
         }
-        float velocityX = Input.GetAxis("Horizontal") * walkStrength;
-        ApplyHorizontalMovement(velocityX);
-        UpdateFacingDirection(velocityX);
 
+        // Here and not inside Move(), because we want to zero velocity when we are not moving.
+        ApplyHorizontalMovement(inputVector.x * walkStrength);
+        // Here and not inside Move(), because we want to know when we get grounded(should reset the jump count).
         TryJump();
+
         Animate();
     }
 
@@ -56,16 +100,16 @@ public class CharacterMovement : MonoBehaviour
 
     void TryJump()
     {
-        float inputY = Input.GetAxisRaw("Vertical");
-        if(inputY > 0 & inputY != lastJumpButtonState & countOfJumps < 2)
+        if (inputVector.y > 0 & inputVector.y != lastJumpButtonState & countOfJumps < 2)
         {
             countOfJumps++;
             body.velocity = new Vector2(body.velocity.x, jumpStrength);
         }
-        else if (inputY < 1 & groundChecker.IsGrounded()) { 
+        else if (inputVector.y < 1 & groundChecker.IsGrounded())
+        {
             countOfJumps = 0;
         }
-        lastJumpButtonState = inputY;
+        lastJumpButtonState = inputVector.y;
     }
 
     public void RegisterArtificialJump()
