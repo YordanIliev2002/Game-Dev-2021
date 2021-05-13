d385decd5bb5c276c6207ccaa3adef3dba87b56a
Yordan Iliev <jordan.iliev501@gmail.com>
2021-05-13 14:55:32 +0300

    Add multiplayer lobby scene

---
+++ Homework-1/Assets/Scripts/Lobby/LobbyManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LobbyManager : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameField;
+    [SerializeField] private TMP_Text errorText;
+
+    public void TryHost()
+    {
+        if (IsNameOk(nameField.text))
+        {
+            HideError();
+            PlayerPrefs.SetString("name", nameField.text);
+            PlayerPrefs.SetString("role", "host");
+            LoadRoom();
+        }
+        else
+        {
+            ShowError("Please enter a valid name.");
+        }
+    }
+
+    public void TryClient()
+    {
+        if (IsNameOk(nameField.text))
+        {
+            HideError();
+            PlayerPrefs.SetString("name", nameField.text);
+            PlayerPrefs.SetString("role", "client");
+            LoadRoom();
+        }
+        else
+        {
+            ShowError("Please enter a valid name.");
+        }
+    }
+
+    private bool IsNameOk(string name)
+    {
+        if(name == null || name.Length == 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void ShowError(string error)
+    {
+        errorText.text = error;
+        errorText.enabled = true;
+    }
+
+    private void HideError()
+    {
+        errorText.enabled = false;
+    }
+
+    private void LoadRoom()
+    {
+        //SceneManager.LoadScene("MultiplayerRoom");
+    }
+
+}
--- Homework-1/Assets/Scripts/MainMenu/MainMenu.cs
+++ Homework-1/Assets/Scripts/MainMenu/MainMenu.cs
@@ -18,4 +18,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("ProceduralLevel");
     }
+
+    public void PlayMultiplayer()
+    {
+        SceneManager.LoadScene("MultiplayerLobby");
+    }
 }
